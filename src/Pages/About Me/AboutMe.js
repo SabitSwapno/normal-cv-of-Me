2ea4cebd281a89d618176242d1e7e09eeefeb164
Sabit Swapno <sabitswapno3@gmail.com>
2022-06-27 02:46:55 +0600

    two section complited

--- src/Pages/About Me/AboutMe.js
+++ src/Pages/About Me/AboutMe.js
@@ -6,7 +6,86 @@ const AboutMe = () => {
     return (
         <>
             <NavigateBar></NavigateBar>
-            This is about me
+            <div style={{
+                marginTop: "0.8rem",
+                marginBottom: "1.2rem",
+            }}>
+                <h1>About <span style={{
+                    color: "#F62455",
+                }}>Me</span></h1>
+                <div className="row text-start ms-2 d-flex justify-content-center align-items-center mt-0 mx-auto">
+                    <div className="col-md-6 col-sm-12">
+                        <h3 className='fw-bold'>Personal Info</h3>
+                        <div className="row text-start">
+                            <div className="col-6">
+                                <h4>First Name</h4>
+                                <h5 className="fw-bolder">Sabit</h5>
+                                <br />
+                                <h4>Age</h4>
+                                <h5 className="fw-bolder">23 Years</h5>
+                                <br />
+                                <h4>Phone</h4>
+                                <h5 className="fw-bolder">+880 1787 040018</h5>
+                                <br />
+                                <h4>Email</h4>
+                                <h5 className="fw-bolder">sabitswapno3@gmail.com</h5>
+                                <br />
+                                <h4>Linked In</h4>
+                                <a target="_blank" style={{
+                                    color: "#F62455",
+                                    fontSize: "18px"
+                                }} href="https://www.linkedin.com/in/sabit-swapno-397850216/">linkedin/sabitswapno</a>
+                            </div>
+                            <div className="col-6">
+                                <h4>Last Name</h4>
+                                <h5 className="fw-bolder">Swapno</h5>
+                                <br />
+                                <h4>Nationality</h4>
+                                <h5 className="fw-bolder">Bangladeshi</h5>
+                                <br />
+                                <h4>Address</h4>
+                                <h5 className="fw-bolder">Banasree D-Block, Rampura, Dhaka</h5>
+                                <br />
+                                <h4>Facebook</h4>
+                                <a target="_blank" style={{
+                                    color: "#F62455",
+                                    fontSize: "18px"
+                                }} href="https://www.facebook.com/Ibne.sabit.swapno">facebook/ibne.sabit.swapno</a>
+                                <br />
+                                <h4 className='pt-4'>Github</h4>
+                                <a target="_blank" style={{
+                                    color: "#F62455",
+                                    fontSize: "18px"
+                                }} href="https://github.com/SabitSwapno">github/sabitswapno</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-sm-12">
+                        <br />
+                        <h3 className='pb-1'>Education</h3>
+                        <h5>BSc in EEE in Green University of Bangladesh.
+                            <br /> • Dept: Electrical & Electronics Engineering
+                            <br /> • Status: On Going (2022-Present)
+                        </h5>
+                        <h5>Graduate from Rajshahi Polytechnic Institute
+                            <br />   • Degree: Diploma Electrical Engineer
+                            <br />  • Duration: 5 years (2016-2021)
+                            <br />  • GPA - 3.09 out of 4</h5>
+                        <h5>
+                            Completed High School from Agrani School and College
+                            <br />
+                            • Class 2 - Class 10
+                            <br /> • Duration: 10 years (2007-2016)
+                            <br /> • GPA - 4.33 out of 5
+                        </h5>
+                        <br />
+                        <h3 className='pb-1'>Languages</h3>
+                        <h5>
+                            • English - Working proficiency
+                            <br /> • Bangla - Native</h5>
+                    </div>
+                </div>
+            </div>
             <Footer></Footer>
         </>
     );
--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -1,12 +1,102 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import Footer from '../Shared/Footer';
 import NavigateBar from '../Shared/NavigateBar';
+import { faUserGroup } from '@fortawesome/free-solid-svg-icons';
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
+import { faListCheck } from '@fortawesome/free-solid-svg-icons';
+import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faComputer } from '@fortawesome/free-solid-svg-icons';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { faGuitar } from '@fortawesome/free-solid-svg-icons';
+import './Skills.css'
 
 const Skills = () => {
     return (
         <>
             <NavigateBar></NavigateBar>
-            this are the Skills
+            <div className='container'>
+                <h1 className='mt-2 mb-4 fw-bold'>My <span style={{
+                    color: "rgb(246, 36, 85)",
+                }}>Skills</span> </h1>
+                <div className="row p-3 g-3 d-flex justify-content-center align-items-center skillsBG mb-3">
+                    <div className='col-md-6 col-sm-12'>
+                        <div className="row d-flex p-3 mb-2 justify-content-center align-items-center smallDivBG">
+                            <div className="col-3">
+                                <h2><FontAwesomeIcon icon={faUserGroup} /></h2>
+                            </div>
+                            <div className="col-9">
+                                <h4 className='fw-bolder'>Strong Communication</h4>
+                                <h6>A cool minded person with strong convincing power which helps to create a great bond with customers.</h6>
+                            </div>
+                        </div>
+                        <div className="row d-flex p-3 mb-2 justify-content-center align-items-center smallDivBG">
+                            <div className="col-3">
+                                <h2><FontAwesomeIcon icon={faMicrophone} /></h2>
+                            </div>
+                            <div className="col-9">
+                                <h4 className='fw-bolder'> Public Speaking</h4>
+                                <h6>A confident personality with clear oral abilities.</h6>
+                            </div>
+                        </div>
+                        <div className="row d-flex p-3 mb-2 justify-content-center align-items-center smallDivBG">
+                            <div className="col-3">
+                                <h2><FontAwesomeIcon icon={faListCheck} /></h2>
+                            </div>
+                            <div className="col-9">
+                                <h4 className='fw-bolder'>Project Management</h4>
+                                <h6>A disciplined person who managed few events & projects by creating schedules and also look after those who finished it on time .</h6>
+                            </div>
+                        </div>
+                        <div className="row d-flex p-3 mb-2 justify-content-center align-items-center smallDivBG">
+                            <div className="col-3">
+                                <h2><FontAwesomeIcon icon={faPeopleGroup} /></h2>
+                            </div>
+                            <div className="col-9">
+                                <h4 className='fw-bolder'>Leadership</h4>
+                                <h6>Bold attitude that helps to managed a group of people many times for various projects.</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-6 col-sm-12'>
+                        <div className="row d-flex p-3 mb-2 justify-content-center align-items-center smallDivBG">
+                            <div className="col-3">
+                                <h2><FontAwesomeIcon icon={faCode} /></h2>
+                            </div>
+                            <div className="col-9">
+                                <h4 className='fw-bolder'>Web-Developing</h4>
+                                <h6>A certified Web Developer who has finished his web-development course from Programming hero.</h6></div>
+                        </div>
+                        <div className="row d-flex p-3 mb-2 justify-content-center align-items-center smallDivBG">
+                            <div className="col-3">
+                                <h2><FontAwesomeIcon icon={faComputer} /></h2>
+                            </div>
+                            <div className="col-9">
+                                <h4 className='fw-bolder'>Computer Literacy</h4>
+                                <h6>Good knowledge about operating computer and digital electronic devices.</h6>
+                            </div>
+                        </div>
+                        <div className="row d-flex p-3 mb-2 justify-content-center align-items-center smallDivBG">
+                            <div className="col-3">
+                                <h2><FontAwesomeIcon icon={faClock} /></h2>
+                            </div>
+                            <div className="col-9">
+                                <h4 className='fw-bolder'>Time Management</h4>
+                                <h6>A disciplined person who always believe to work in time to time.</h6>
+                            </div>
+                        </div>
+                        <div className="row d-flex p-3 mb-2 justify-content-center align-items-center smallDivBG">
+                            <div className="col-3">
+                                <h2><FontAwesomeIcon icon={faGuitar} /></h2>
+                            </div>
+                            <div className="col-9">
+                                <h4 className='fw-bolder'>Musical knowledge</h4>
+                                <h6>Have a good knowledge about music and also knows how to play guitars.</h6></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer></Footer>
         </>
     );
