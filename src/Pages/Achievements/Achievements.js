065dd3ba159e715d521d5db57ac9e496d272551a
Sabit Swapno <sabitswapno3@gmail.com>
2022-06-28 20:10:45 +0600

    done for now

--- src/Pages/Achievements/Achievements.js
+++ src/Pages/Achievements/Achievements.js
@@ -1,12 +1,74 @@
 import React from 'react';
 import Footer from '../Shared/Footer';
 import NavigateBar from '../Shared/NavigateBar';
+import './Achievements.css'
 
 const Achievements = () => {
     return (
         <>
             <NavigateBar></NavigateBar>
-            this is the achievements.
+            <div className='container'>
+                <h1 className='mt-2 mb-3 fw-bold'>Achiev<span style={{
+                    color: "rgb(246, 36, 85)",
+                }}>ements</span> </h1>
+                <div className='container'>
+                    <div className="card mb-3 cardbgCus">
+                        <div className="row g-0 justify-content-center align-items-center">
+                            <div className="col-md-4 imgset">
+                                <img style={{
+                                    width: "50%"
+                                }} src="https://i0.wp.com/educationsinbd.com/wp-content/uploads/2018/09/Educationsinbd-pic-4-post-61.jpg?resize=800%2C445&ssl=1" className="img-fluid rounded-start" alt="..." />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h2 className="card-title">National Skill Standard Course</h2>
+                                    <h6 className="card-text">Passed as a "General Electrician" in
+                                        National Skill Standard Basic Course</h6>
+                                    <h6 className="card-text"><small className="text-muted">-2018</small></h6>
+                                    <a className='Custombutton1' href="https://drive.google.com/file/d/1cQg8QGjMHMU3GizILmLzZe97QglFJdCH/view?usp=sharing" target="_blank">View Certificate</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card mb-3 cardbgCus">
+                        <div className="row g-0 justify-content-center align-items-center">
+                            <div className="col-md-4 imgset">
+                                <img style={{
+                                    width: "65%"
+                                }} src="https://cdn.dribbble.com/users/1488337/screenshots/6944906/programming_hero_logo.jpg?compress=1&resize=400x300&vertical=top" className="img-fluid rounded-start" alt="..." />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h2 className="card-title">Web Developing</h2>
+                                    <h6 className="card-text">Completed Web-Development course
+                                        with high average marks in
+                                        Programming hero</h6>
+                                    <h6 className="card-text"><small className="text-muted">-2021</small></h6>
+                                    <a className='Custombutton1' href="https://drive.google.com/file/d/1zLi7oIOVt7CR_OinxAUEpNR1uLpFq8_O/view?usp=sharing" target="_blank">View Certificate</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card mb-3 cardbgCus">
+                        <div className="row g-0 justify-content-center align-items-center">
+                            <div className="col-md-4 imgset">
+                                <img style={{
+                                    width: "45%"
+                                }} src="https://i.ibb.co/Z6MnMjf/achievement.jpg" className="img-fluid rounded-start" alt="..." />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h2 className="card-title">Song Submission</h2>
+                                    <h6 className="card-text">Participated and submitted a song to
+                                        an Album called "Oboseshe" with
+                                        band Enharmonic</h6>
+                                    <h6 className="card-text"><small className="text-muted">-2019</small></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer></Footer>
         </>
     );
--- src/Pages/Experience/Experiences.js
+++ src/Pages/Experience/Experiences.js
@@ -12,50 +12,50 @@ const Experiences = () => {
                     color: "rgb(246, 36, 85)",
                 }}>Experiences</span> </h1>
                 <div className='container'>
-                    <div class="card mb-3 cardbgC">
-                        <div class="row g-0 justify-content-center align-items-center">
-                            <div class="col-md-4 imgset">
+                    <div className="card mb-3 cardbgCus">
+                        <div className="row g-0 justify-content-center align-items-center">
+                            <div className="col-md-4 imgset">
                                 <img style={{
                                     width: "50%"
-                                }} src="https://i.ibb.co/gMPZ3vV/Metal-Carnival.jpg" class="img-fluid rounded-start" alt="..." />
+                                }} src="https://i.ibb.co/gMPZ3vV/Metal-Carnival.jpg" className="img-fluid rounded-start" alt="..." />
                             </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h2 class="card-title">Metal Carnival</h2>
-                                    <h6 class="card-text">ENVENOM and ENHARMONIC aligned together to organize this event. So, that could be the biggest underground show that Rajshahi has never seen before!!!!</h6>
-                                    <h6 class="card-text"><small class="text-muted">-2019</small></h6>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h2 className="card-title">Metal Carnival</h2>
+                                    <h6 className="card-text">ENVENOM and ENHARMONIC aligned together to organize this event. So, that could be the biggest underground show that Rajshahi has never seen before!!!!</h6>
+                                    <h6 className="card-text"><small className="text-muted">-2019</small></h6>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="card mb-3 cardbgC">
-                        <div class="row g-0 justify-content-center align-items-center">
-                            <div class="col-md-4 imgset">
+                    <div className="card mb-3 cardbgCus">
+                        <div className="row g-0 justify-content-center align-items-center">
+                            <div className="col-md-4 imgset">
                                 <img style={{
                                     width: "65%"
-                                }} src="https://i.ibb.co/k4pdr1t/new-cover3-png-1.jpg" class="img-fluid rounded-start" alt="..." />
+                                }} src="https://i.ibb.co/k4pdr1t/new-cover3-png-1.jpg" className="img-fluid rounded-start" alt="..." />
                             </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h2 class="card-title">Bring back a Smile</h2>
-                                    <h6 class="card-text">The Agrani School's 16th Batch & Band Enharmonic organized this charity show for a Blood Cancer patient. After the show they was able to give the patient a decent amount of money for her treatment.</h6>
-                                    <h6 class="card-text"><small class="text-muted">-2022</small></h6>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h2 className="card-title">Bring back a Smile</h2>
+                                    <h6 className="card-text">The Agrani School's 16th Batch & Band Enharmonic organized this charity show for a Blood Cancer patient. After the show they was able to give the patient a decent amount of money for her treatment.</h6>
+                                    <h6 className="card-text"><small className="text-muted">-2022</small></h6>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="card mb-3 cardbgC">
-                        <div class="row g-0 justify-content-center align-items-center">
-                            <div class="col-md-4 imgset">
+                    <div className="card mb-3 cardbgCus">
+                        <div className="row g-0 justify-content-center align-items-center">
+                            <div className="col-md-4 imgset">
                                 <img style={{
                                     width: "65%"
-                                }} src="https://i.ibb.co/HChF44S/Summer-Combat.jpg" class="img-fluid rounded-start" alt="..." />
+                                }} src="https://i.ibb.co/HChF44S/Summer-Combat.jpg" className="img-fluid rounded-start" alt="..." />
                             </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h2 class="card-title">Summer Combat</h2>
-                                    <h6 class="card-text">Genesis Music Home is going to present an underground concert for metal heads of Rajshahi. The main motto of this UG is promoting some new talented bands with some familiar bands of Rajshahi.</h6>
-                                    <h6 class="card-text"><small class="text-muted">-2019</small></h6>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h2 className="card-title">Summer Combat</h2>
+                                    <h6 className="card-text">Genesis Music Home is going to present an underground concert for metal heads of Rajshahi. The main motto of this UG is promoting some new talented bands with some familiar bands of Rajshahi.</h6>
+                                    <h6 className="card-text"><small className="text-muted">-2019</small></h6>
                                 </div>
                             </div>
                         </div>
